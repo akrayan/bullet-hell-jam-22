46461c15be6e68d81bdbfb9b6ae7878bf2c2b6aa
Raihane AKKACHE <raihane.akkache@gmail.com>
2022-04-17 11:43:04 +0200

    link life ui to player's life

--- Assets/Scripts/LifeUI.cs
+++ Assets/Scripts/LifeUI.cs
@@ -10,19 +10,14 @@ public class LifeUI : MonoBehaviour
     [SerializeField] private Slider lifeSlider;
     [SerializeField] private TextMeshProUGUI lifeNumber;
     
-    // exemple Event
-    //public UnityAction<float> onDamaged { get; set; }
     
-    // Start is called before the first frame update
     void Start()
-    {
-        lifeSlider.value = 0.5f;
-        lifeNumber.text = "50 / 100";
-        //search player, register mon callbak when health change
+    {        
+        playerHealth.onLifeChange += onLifeUpdate;
     }
 
     void onLifeUpdate(int currentLife, int maxLife) {
         lifeNumber.text = "" + currentLife + " / " + maxLife ;
+        lifeSlider.value = (float)currentLife / (float)maxLife;
     }
-
 }
